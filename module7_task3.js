40ab0639edfefa3bac3839b6cf10eab824313488
liza <lizadzundiet@gmail.com>
2024-07-22 20:47:01 +0300

    Удалены все добавленные файлы

--- module7_task3.js
+++
@@ -1,9 +0,0 @@
-const users = [
-  {name:'Sasha', age:21},
-  {name:'Dasha', age:18},
-  {name:'Masha', age:15},
-  {name:'Pasha', age:19}
-]
-
-const newUsers = users.filter(x => x.age >= 18);
-console.log(newUsers);
